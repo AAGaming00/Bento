8ce67e846259dcac46c48011ee8368531fec2da5
AAGaming00 <aa@bigdumb.gq>
2020-11-25 21:15:13 -0500

    fix zoom prop

--- src/components/Panels/iframe.js
+++ src/components/Panels/iframe.js
@@ -3,8 +3,11 @@ import { h } from 'preact';
 export default function IFrameCard ({panel}) {
     return <iframe
     className = 'qlist__iframe'
-    style = {{
-        zoom: panel.zoom?.toString()
+    style = {panel.zoom && {
+        transform: `scale(${panel.zoom?.toString()})`,
+        width: `${1 / panel.zoom * 100}%`,
+        height: `${1 / panel.zoom * 100}%`,
+        transformOrigin: 'top'
     }}
     src={panel.url}
     sandbox = 'allow-downloads allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-presentation allow-scripts allow-top-navigation' />
